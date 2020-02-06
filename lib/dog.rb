53677e09f6e1c8e0f723bf9fc1bb3a23aae79c07
Alex Lee <hyunho98@gmail.com>
2020-02-06 02:03:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -64,6 +64,9 @@ class Dog
       dog = self.new(name: name, breed: breed)
     else
       dog_data = dog.first
-      dog = Dog.new(dog_data[0],)
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
+    end
+    dog
+  end
 
 end
