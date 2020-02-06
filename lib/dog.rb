1f5b03294fd83e017770d5aaa71c079d97e157bc
Alex Lee <hyunho98@gmail.com>
2020-02-06 01:51:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -23,4 +23,13 @@ class Dog
   def self.drop_table
     DB[:conn].execute("DROP TABLE IF EXISTS dogs")
   end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO dogs (name, breed)
+      VALUES (?, ?);
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
 end
